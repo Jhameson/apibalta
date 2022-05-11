b6d4ac4be93b0d33e7ac15ac1cad86c837ff9ee1
fit-jhameson-farias <jhameson.farias@fitbank.com.br>
2022-05-11 15:48:47 -0300

    first commit

---
+++ Data/DataContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using apibalta.Models;
+
+namespace apibalta.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
